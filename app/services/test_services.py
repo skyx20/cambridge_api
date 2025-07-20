bc4b24aeb1b13a62a63ce664dc16f21f0d72a2f8
skyx20 <dc.linkups@gmail.com>
2025-07-20 18:02:46 -0300

    fix(Parser): Fix audio_link extraction bug when there's no audio link

--- app/services/test_services.py
+++ app/services/test_services.py
@@ -2,32 +2,14 @@ from .fetcher import Fetcher
 from .parser import Parser
 from app.utils.utils import get_html_page
 from pprint import pp, pprint
-from app.services.dictionary import Dictionary
-from fastapi.exceptions import HTTPException 
-from fastapi.responses import JSONResponse 
-from typing import Literal
-
 
 if __name__ == "__main__":
     f = Fetcher()
     # page = f.get_word_page('option')
     # p = Parser(page)
-    # page = get_html_page('option')
-    # p = Parser(page)
-    # p.select_dictionary('us')
-    # w = p.parse_meanings()
-    # pprint(w.to_json())
-    word = 'option'
-    dict_variant:Literal['uk', 'us', 'be'] = 'uk'
-    def search_word(word: str, dict_variant:Literal['uk', 'us', 'be']):
-        d = Dictionary(dict_variant)
-        try:
-            w = d.search_meaning(word)
-        except Exception as e:
-            print(e)
-            raise HTTPException(status_code=404, detail=e.__str__())
-        else:
-            word_data = w.to_dict()
-            return JSONResponse(word_data, status_code=200)
-            
-    search_word(word, dict_variant)
+    page = get_html_page('option')
+    p = Parser(page)
+    p.select_dictionary('uk')
+    w = p.parse_meanings()
+    pprint(w.to_json())
+  
